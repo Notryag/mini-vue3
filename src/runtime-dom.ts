9b5d644e751555c3703ddb256f4569093a381db1
Notryag <1135814285@qq.com>
2021-01-31 23:26:17 +0800

    init

---
+++ src/runtime-dom.ts
@@ -0,0 +1,36 @@
+export const nodeOps = {
+  insert:(child,container:Element,anchor) => {
+      if(anchor) {
+        container.insertBefore(child,anchor)
+      }else {
+        container.appendChild(child)
+      }
+  },
+  remove: (child:Element) => {
+    const parent = child.parentNode
+    if(parent) {
+      parent.removeChild(child)
+    }
+  },
+
+  createElement: tag => document.createElement(tag),
+
+  setElementText: (el:Element,text) => {
+    el.textContent = text
+  }
+}
+const onRe = /^on[^a-z]/
+const isOn = key => onRe.test(key)
+export function hostPatchProps(el:Element,key,prev,next) {
+  if(isOn) {
+    const name = key.slice(2).toLowerCase()
+    prev && el.removeEventListener(name,prev)
+    next && el.addEventListener(name,next)
+  }else {
+    if(next === null) {
+      el.removeAttribute(key)
+    }else {
+      el.setAttribute(key,next)
+    }
+  }
+}
