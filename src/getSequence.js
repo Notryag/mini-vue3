0bfc93956a92f17e8adc0fe47249107a3775ee04
Notryag <1135814285@qq.com>
2021-02-05 00:35:56 +0800

    完成getSequence

--- src/getSequence.js
+++ src/getSequence.js
@@ -1,31 +1,43 @@
 //todo 最长子序列
 function getSequence(arr) {
   const result = [0]
-
-  let i, j, u, v, c
+  let i, j, u, v
+  const p = arr.slice()
   const len = arr.length
-  for(i=0;i<len ;i++) {
+  for (i = 0; i < len; i++) {
     let arrI = arr[i]
     j = result[result.length - 1]
-    if(arrI > arr[j]) {
+    if (arrI > arr[j]) {
+      p[i] = j
       result.push(i)
     }
-
     u = 0
     v = result.length - 1
-    while(u < v) {
-      let c = (u + v) / 2 | 0
-      if(arr[result[c]] < arrI) {
+    while (u < v) {
+      c = ((u + v) / 2) | 0
+      if (arrI > arr[c]) {
         u = c + 1
-      }else {
+      } else {
         v = c
       }
     }
-    // 找到比arrI大的值, 并且是极限(就是数组中大的最少的),相当于 插入有序数组,并使数组依然有序
-    result[u] = i
+    if (arrI < arr[result[u]]) {
+      if (u > 0) {
+        p[i] = result[u - 1]
+      }
+      result[u] = i
+    }
+  }
+  u = result.length
+  v = result[u - 1]
+  while (u-- > 0) {
+    result[u] = v
+    v = p[v]
   }
   return result
 }
-                      // 0   1  2  3  4  5  6  7
+// 0   1  2  3  4  5  6  7
 console.log(getSequence([1, 2, 5, 3, 0, 4, 7, 6]))
 // 1 3 6
+// 了解贪心 + 二分法  
+// 其中的p  作为存储数组,了解其思想
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,115 @@
-import {nodeOps,hostPatchProps} from './runtime-dom'
+import { nodeOps, hostPatchProps } from './runtime-dom'
 
 export * from './reactivity'
 export * from './runtime-dom'
 
 export function render(vnode, container) {
-  console.log('render');
-  
-  patch(container._vnode|| null , vnode, container)
+  console.log('render')
+
+  patch(container._vnode || null, vnode, container)
   container._vnode = vnode
 }
 
-export function patch(n1,n2,container){
-  if(typeof n2.type === 'string') {
-    processElment(n1,n2,container)
+export function patch(n1, n2, container) {
+  if (typeof n2.type === 'string') {
+    processElment(n1, n2, container)
   }
 }
 
-export function processElment(n1,n2,container) {
-  if(n1 == null) {
-    mountElement(n2,container)
+export function processElment(n1, n2, container) {
+  if (n1 == null) {
+    mountElement(n2, container)
   } else {
-    patchElement(n1,n2,container)
+    patchElement(n1, n2, container)
   }
 }
 
-function patchElement(n1,n2,contianer) {
-  
+function patchElement(n1, n2, contianer) {
+  let el = (n2.el = n1.el)
+  let oldProps = n1.props
+  let newProps = n2.props
+
+  // 比较props
+  patchProps(el, n2, oldProps, newProps)
+
+  patchChildren(n1, n2, el)
 }
 
-function mountElement(vnode,container) {
-  const { type,props,children} = vnode
+function patchProps(el, n2, oldProps, newProps) {
+  if (oldProps !== newProps) {
+    // 新的比旧的多
+    for (const key in newProps) {
+      const prev = oldProps[key]
+      const next = newProps[key]
+
+      hostPatchProps(el, key, prev, next)
+    }
+    // 旧的多 新的少
+    for (const key in oldProps) {
+      if (!(key in newProps)) {
+        hostPatchProps(el, key, oldProps[key], null)
+      }
+    }
+  }
+}
+
+function patchChildren(n1, n2, container) {
+  const c1 = n1 && n1.children
+  const c2 = n2.children
+  if (typeof c2.type === 'string') {
+    // arr > string
+    if (Array.isArray(c1)) {
+      unmountChildren(c1)
+    }
+    //  str > str
+    if (c1 !== c2) {
+      nodeOps.setElementText(container, c2)
+    }
+  } else {
+    // str > arr
+    if (typeof c1.type === 'string') {
+      nodeOps.setElementText(container, '')
+      mountChildren(c2, container)
+    }
+    // arr > arr
+    patchKeyedChildren(c1, c2, container)
+  }
+}
+
+function patchKeyedChildren(c1, c2, container) {
+  unmountChildren(c1)
+  mountChildren(c2, container)
+}
+
+function unmountChildren(children) {
+  for (let i = 0; i < children; i++) {
+    unmount(children[i])
+  }
+}
+
+function unmount(child) {
+  nodeOps.remove(child)
+}
+
+function mountElement(vnode, container) {
+  const { type, props, children } = vnode
   let el = (vnode.el = nodeOps.createElement(type))
 
-  if(props) {
-    for( const key in props) {
-      hostPatchProps(el,key,null, props[key])
+  if (props) {
+    for (const key in props) {
+      hostPatchProps(el, key, null, props[key])
     }
   }
 
-  if(typeof children ==='string') {
-    nodeOps.setElementText(el,children)  
-  }else if(Array.isArray(children)){
-    mountChildren(children,el)
+  if (typeof children === 'string') {
+    nodeOps.setElementText(el, children)
+  } else if (Array.isArray(children)) {
+    mountChildren(children, el)
   }
-  nodeOps.insert(el, container,null)
+  nodeOps.insert(el, container, null)
 }
 
-function mountChildren(children,el) {
+function mountChildren(children, el) {
   for (let i = 0; i < children.length; i++) {
     patch(null, children[i], el)
   }
